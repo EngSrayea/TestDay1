97bf3026f352929757aab571322a99b3d4b760fb
EngSrayea <eng.nsrayea@gmail.com>
2020-05-01 21:37:38 +0300

    Initial Commit

---
+++ TestDay1/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TestDay1
+//
+//  Created by Eng. Norii on 5/1/20.
+//  Copyright © 2020 EngSrayea. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
